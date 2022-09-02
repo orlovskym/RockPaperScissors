080ee627599fb4c80629ae16423f891a2f3e921c
Mark Orlovsky <orlovskym@gmail.com>
2022-09-01 18:31:01 -0700

    add game() to make a best-of-5

--- javascript.js
+++ javascript.js
@@ -22,21 +22,46 @@ function getPlayerChoice() {
 
 function rockPaperScissors(computer, player) {
     if (computer == player) {
-        return ("Draw, both players used "+computer+".")
+        console.log("Draw, both players used " + computer + ".")
+        return 0
     } else if (computer == "rock" & player == "paper") {
-        return "You win, paper covers rock."
+        console.log("You win, paper covers rock.")
+        return 1
     } else if (computer == "rock" & player == "scissors") {
-        return "You lose, rock smashes scissors."
+        console.log("You lose, rock smashes scissors.")
+        return 2
     } else if (computer == "paper" & player == "rock") {
-        return "You lose, paper covers rock."
+        console.log("You lose, paper covers rock.")
+        return 2
     } else if (computer == "paper" & player == "scissors") {
-        return "You win, scissors cuts paper."
+        console.log("You win, scissors cuts paper.")
+        return 1
     } else if (computer == "scissors" & player == "paper") {
-        return "You lose, scissors cuts paper."
+        console.log("You lose, scissors cuts paper.")
+        return 2
     } else if (computer == "scissors" & player == "rock") {
-        return "You win, rock smashes scissors."
+        console.log("You win, rock smashes scissors.")
+        return 1
     }
 }
 
-console.log(rockPaperScissors(getComputerChoice(), getPlayerChoice()))
-//console.log(getPlayerChoice())
+function game() {
+    let draws = 0
+    let playerwins = 0
+    let computerwins = 0
+
+    for (let i = 0; i < 5; i++) {
+        let result = rockPaperScissors(getComputerChoice(), getPlayerChoice())
+        if (result == 0) {
+            draws++
+        } else if (result == 1) {
+            playerwins++
+        } else if (result == 2) {
+            computerwins++
+        }
+    }
+
+    console.log("In 5 rounds you got " + draws + " draws, " + playerwins + " wins, and " + computerwins + " losses.")
+}
+
+game()
